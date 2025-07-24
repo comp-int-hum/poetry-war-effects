e3d5d8745164010c337cd547a0e643a66a0d8ed0
jzhan299 <jzhan299@login03.cm.cluster>
2025-07-23 21:04:22 -0400

    added word count dictionaries

--- script/inspect_models.py
+++ script/inspect_models.py
@@ -1,16 +1,27 @@
 from gensim.models import Word2Vec
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i1','--input_prewar',dest = 'input_prewar',help = 'trained prewar model')
 parser.add_argument('-i2','--input_postwar',dest = 'input_postwar',help = 'trained postwar model')
+parser.add_argument('-i3','--prewar_wordcount',dest = 'prewar_wordcount',help = 'dictionary of words used in prewar poetry')
+parser.add_argument('-i4','--postwar_wordcount',dest = 'postwar_wordcount',help = 'dictionary of words used in postwar poetry')
 parser.add_argument('-w',dest = 'word',help = 'this is the word to examine')
 args = parser.parse_args()
 
 model_prewar = Word2Vec.load(args.input_prewar)
 model_postwar = Word2Vec.load(args.input_postwar)
-sims_prewar = model_prewar.wv.most_similar(args.word,topn = 10)
-sims_postwar = model_postwar.wv.most_similar(args.word,topn = 10)
 
-print(sims_prewar)
-print(sims_postwar)
+with open(args.prewar_wordcount,'r') as ifd_prewar:
+    dict_prewar = json.load(ifd_prewar)
+with open(args.postwar_wordcount,'r') as ifd_postwar:
+    dict_postwar = json.load(ifd_postwar)
+
+if dict_prewar[args.word] < 10 or dict_postwar[args.word] < 10:
+    print('Insufficient use of word. Please choose another word.')
+else:
+    sims_prewar = model_prewar.wv.most_similar(args.word,topn = 10)
+    sims_postwar = model_postwar.wv.most_similar(args.word,topn = 10)
+    print(sims_prewar)
+    print(sims_postwar)
--- script/train_models.py
+++ script/train_models.py
@@ -10,6 +10,8 @@ parser.add_argument('-i1','--input_prewar',dest = 'input_prewar',help = 'prewar 
 parser.add_argument('-i2','--input_postwar',dest = 'input_postwar',help = 'postwar jsonlines file')
 parser.add_argument('-o1','--output_prewar',dest = 'output_prewar',help = 'prewar model')
 parser.add_argument('-o2','--output_postwar',dest = 'output_postwar',help = 'postwar model')
+parser.add_argument('-o3','--prewar_vocab',dest = 'prewar_vocab',help = 'dictionary of all words used to train prewar model')
+parser.add_argument('-o4','--postwar_vocab',dest = 'postwar_vocab',help = 'dictionary of all words used to train postwar model')
 args = parser.parse_args()
 
 word_list_prewar = []
@@ -26,6 +28,16 @@ with gzip.open(args.input_prewar,'r') as ifd_prewar:
                 word_list_per_sentence_prewar = sentence_prewar.split(' ')
                 word_list_prewar.append(word_list_per_sentence_prewar)
 
+dict_prewar = {}
+for line in word_list_prewar:
+    for word in line:
+        if word not in dict_prewar:
+            dict_prewar[word] = 1
+        else:
+            dict_prewar[word] += 1
+with open(args.prewar_vocab,'w') as ifd_prewar_wordcount:
+    json.dump(dict_prewar,ifd_prewar_wordcount)
+
 with gzip.open(args.input_postwar,'r') as ifd_postwar:
     for line_postwar in ifd_postwar:
         poem_postwar = json.loads(line_postwar)
@@ -36,6 +48,15 @@ with gzip.open(args.input_postwar,'r') as ifd_postwar:
                 sentence_postwar = re.sub(r'[^\w\s]', '', initial_processed_sentence_postwar)
                 word_list_per_sentence_postwar = sentence_postwar.split(' ')
                 word_list_postwar.append(word_list_per_sentence_postwar)
+dict_postwar = {}
+for line in word_list_postwar:
+    for word in line:
+        if word not in dict_postwar:
+            dict_postwar[word] = 1
+        else:
+            dict_postwar[word] += 1
+with open(args.postwar_vocab,'w') as ifd_postwar_wordcount:
+    json.dump(dict_postwar,ifd_postwar_wordcount)
 
 model_prewar = Word2Vec(sentences = word_list_prewar, vector_size = 100, window = 5, min_count = 1, workers = 4)
 model_prewar.save(args.output_prewar)
