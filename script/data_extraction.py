9a11b3485ccbef17994820c2a927403f0dc102af
jzhan299 <jzhan299@login03.cm.cluster>
2025-08-07 13:10:41 -0400

    started to modify data_extraction.py

--- script/data_extraction.py
+++ script/data_extraction.py
@@ -1,8 +1,22 @@
 import json
 import gzip # built-in decompression tool
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset',dest = 'dataset',default ='/scratch4/tlippin1/data/chadwyck_new.jsonl.gz',help = 'dataset tp examine')
+parser.add_argument('--latest_year_prewar',dest = 'latest_year_prewar',type = int, default = 1864,help = 'the latest year authors are born, so that their works are considered prewar')
+parser.add_argument('--earliest_year_postwar',dest = 'earliest_year_postwar',type = int, default = 1904,help = 'the earliest year authors are born, so that their works are considered postwar')
+parser.add_argument('--prewar_from',dest = 'prewar_from',type = int, default = 1893,help = 'from what year the poems are considered prewar')
+parser.add_argument('--prewar_to',dest = 'prewar_to',type = int, default = 1913,help = 'to what year the poems are considered prewar')
+parser.add_argument('--postwar_from',dest = 'postwar_from',type = int, default = 1919,help = 'from what year the poems are considered postwar')
+parser.add_argument('--postwar_to',dest = 'postwar_to',type = int, default = 1939,help = 'to what year the poems are considered postwar')
+parser.add_argument('--output_prewar',dest = 'output_prewar',default = 'work/prewar_poetry.jsonl.gz',help = 'jsonlines file of prewar poetry')
+parser.add_argument('--output_postwar',dest = 'output_postwar',default = 'work/postwar_poetry.jsonl.gz',help = 'jsonlines file of postwar poetry')
+args = parser.parse_args()
 
 
-with gzip.open('/scratch4/tlippin1/data/chadwyck_new.jsonl.gz','r') as ifd, gzip.open('/home/jzhan299/poetry_project/work/prewar_poetry.jsonl.gz','w') as ofd_pre, gzip.open('/home/jzhan299/poetry_project/work/postwar_poetry.jsonl.gz','w') as ofd_post:
+with gzip.open(args.dataset,'r') as ifd, gzip.open(args.output_prewar,'w') as ofd_pre, gzip.open(args.output_postwar,'w') as ofd_post:
     count_pre_war = 0
     count_post_war = 0
 
@@ -17,7 +31,7 @@ with gzip.open('/scratch4/tlippin1/data/chadwyck_new.jsonl.gz','r') as ifd, gzip
             if poem['original_metadata']['year'] != '0':
                 year_written = int(poem['original_metadata']['year'])
             else:
-                if int(author_dob) <= 1864 or int(author_dob) >= 1904: # author is born at least 50 years before WWI (prewar), or author is never drafted into WWI (postwar)
+                if int(author_dob) <= args.latest_year_prewar or int(author_dob) >= args.earliest_year_postwar: # author is born at least 50 years before WWI (prewar), or author is never drafted into WWI (postwar)
                     year_written = int(author_dob) + 35
                 else:
                     year_written = 0
@@ -25,10 +39,10 @@ with gzip.open('/scratch4/tlippin1/data/chadwyck_new.jsonl.gz','r') as ifd, gzip
             # check year_written conditions
             # write into jsonlines file
             # count number of poems in each category
-            if year_written >= 1893 and year_written <= 1913:
+            if year_written >= args.prewar_from and year_written <= args.prewar_to:
                 count_pre_war += 1
                 ofd_pre.write(line)
-            elif year_written >= 1919 and year_written <= 1939:
+            elif year_written >= args.postwar_from and year_written <= args.postwar_to:
                 count_post_war += 1
                 ofd_post.write(line)
 
